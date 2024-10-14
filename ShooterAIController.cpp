127a28e7ba483db6aeda84e93a097c308e92276f
YouYeDam <youyedam@naver.com>
2024-10-15 00:34:00 +0900

    심플슈터 코드 업로드

---
+++ ShooterAIController.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "ShooterCharacter.h"
+
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (AIBehavior != nullptr) {
+        RunBehaviorTree(AIBehavior);
+
+        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+    }
+}
+
+void AShooterAIController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+}
+
+bool AShooterAIController::IsDead() const
+{
+    AShooterCharacter* ControlledCharacter = Cast<AShooterCharacter>(GetPawn());
+    if (ControlledCharacter != nullptr) {
+        return ControlledCharacter->IsDead();
+    }
+
+    return true; //폰을 컨트롤하고 있지 않다면 이 컨트롤러는 사망한 것이나 다름없음.
+}
